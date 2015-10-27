18cc74dda6a0fc1250d53e200f2105f37b54457c
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-10-27 13:14:37 +1100

    Add ui test

---
+++ UITestTextViewDemoUITests/UITestTextViewDemoUITests.swift
@@ -0,0 +1,25 @@
+//
+//  UITestTextViewDemoUITests.swift
+//  UITestTextViewDemoUITests
+//
+//  Created by Evgenii on 27/10/2015.
+//  Copyright © 2015 Marketplacer. All rights reserved.
+//
+
+import XCTest
+
+class UITestTextViewDemoUITests: XCTestCase {
+  
+    
+    func testExample() {
+      continueAfterFailure = false
+      let app = XCUIApplication()
+      app.launch()
+      
+      var textView = app.textViews.elementBoundByIndex(0)
+      textView.tap()
+      textView.typeText("Hello world")
+      XCTAssertEqual("Hello world", textView.value as? String)
+    }
+    
+}
