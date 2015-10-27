2e27ed80a0f0bced55e7df962df7440cc6f74abf
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-10-27 17:14:58 +1100

    Fix test

--- UITestTextViewDemoUITests/UITestTextViewDemoUITests.swift
+++ UITestTextViewDemoUITests/UITestTextViewDemoUITests.swift
@@ -9,11 +9,8 @@ class UITestTextViewDemoUITests: XCTestCase {
     let textField = app.textFields.elementBoundByIndex(0)
     textField.tap()
     textField.typeText("hello")
-    
-    XCTAssertEqual(1, app.textViews.count)
-    
+        
     let textView = app.textViews.elementBoundByIndex(0)
-    textView.swipeDown()
     textView.tap()
     XCUIApplication().textViews.elementBoundByIndex(0).typeText("Hello world")
   }
